5cdde3aec051591a789731cbdd0d8672ac436bca
charlestian23 <charles.tian23@gmail.com>
2022-07-18 16:08:05 -0400

    Rebuilt without build folder

--- build/MathFunctions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charles/OpenSourceSoftware/cmake-lab8-checkpoint4/Step5/MathFunctions
-# Build directory: /home/charles/OpenSourceSoftware/cmake-lab8-checkpoint4/build/MathFunctions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
